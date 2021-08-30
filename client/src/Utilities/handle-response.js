ec33ed65fc33a718adc975b55e18a1ea2df7af22
anandadhikari <anandadhikari02@gmail.com>
2021-08-30 12:39:37 +0530

    update

--- client/src/Utilities/handle-response.js
+++ client/src/Utilities/handle-response.js
@@ -7,7 +7,6 @@ const useHandleResponse = () => {
   const handleResponse = (response) => {
     return response.text().then((text) => {
       const data = text && JSON.parse(text);
-      console.log(response, "response");
       if (!response.ok) {
         if ([401, 403].indexOf(response.status) !== -1) {
           authenticationService.logout();
@@ -19,6 +18,7 @@ const useHandleResponse = () => {
         const error = (data && data.message) || response.statusText;
         return Promise.reject(error);
       }
+      console.log(data, "response");
 
       return data;
     });
