4ff3bd92a1538f50576d896803f5339ef63f554d
anandadhikari <anandadhikari02@gmail.com>
2021-08-30 12:28:40 +0530

    update

--- client/src/Utilities/handle-response.js
+++ client/src/Utilities/handle-response.js
@@ -7,7 +7,7 @@ const useHandleResponse = () => {
   const handleResponse = (response) => {
     return response.text().then((text) => {
       const data = text && JSON.parse(text);
-
+      console.log(response, "response");
       if (!response.ok) {
         if ([401, 403].indexOf(response.status) !== -1) {
           authenticationService.logout();
