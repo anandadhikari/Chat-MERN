2296ae0a7a5dacf927fda775bbbae9dc84539c56
anandadhikari <anandadhikari02@gmail.com>
2021-08-31 19:23:20 +0530

    updates

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI: "mongodb+srv://anand:anand123@cluster0.koxwh.mongodb.net/chat",
-  secretOrKey: "secret",
+  secretOrKey: "123-456-789",
 };
--- server.js
+++ server.js
@@ -11,13 +11,6 @@ const path = require("path");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8080",
-  optionsSuccessStatus: 200,
-  methods: "GET, PUT",
-};
-
-app.use(cors(corsOptions));
 // Port that the webserver listens to
 const port = process.env.PORT || 8000;
 
@@ -46,7 +39,7 @@ app.use(
 app.use(bodyParser.json());
 
 // CORS middleware
-// app.use(cors());
+app.use(cors());
 
 // Database configuration
 const db = require("./config/keys").mongoURI;
